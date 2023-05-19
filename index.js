dd2bc8684eeb4090b8106bae435427d9cebf9a5d
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2023-05-20 00:37:17 +0600

    added vercel.json

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const toysCollection = client.db("dollToysDB").collection("toys");
 
@@ -138,6 +138,7 @@ async function run() {
     // await client.close()
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
