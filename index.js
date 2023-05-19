f7f824ad0bce1642a851aa3f997fe21f3d909269
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2023-05-19 21:37:31 +0600

    crated user delete his data api

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ async function run() {
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connectio
+    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
